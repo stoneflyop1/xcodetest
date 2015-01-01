1d9f046ebaa49f2249b9f0f4fbaf4ddaa476c55e
jeff <jefflyop1@gmail.com>
2015-01-01 15:50:57 +0800

    change tests to functions...

--- proj1/proj1/main.m
+++ proj1/proj1/main.m
@@ -12,49 +12,65 @@
 #import "Rectangle.h"
 #import "Square.h"
 
+void testNumbers() {
+    Fraction *aF;
+    Fraction *bF;
+    Fraction *cF;
+    
+    NSLog(@"Fraction Count: %i", [Fraction count]);
+    //myFraction = [[Fraction alloc] init];
+    aF = [[Fraction allocF] init];
+    NSLog(@"Fraction Count: %i", [Fraction count]);
+    bF = [[Fraction allocF] init];
+    NSLog(@"Fraction Count: %i", [Fraction count]);
+    [aF setTo:6 over:8];
+    [bF setTo:5 over:9];
+    //myFraction.numerator = 1.0;
+    //myFraction.denominator = 3.0;
+    
+    [aF print];
+    
+    [aF reduce];
+    
+    [aF print];
+    
+    cF = [aF add: bF];
+    
+    [cF print];
+    
+}
+
+void testShapes() {
+    
+    Point2D *p;
+    Rectangle *r;
+    Square *s;
+    
+    
+    
+    p = [Point2D new];
+    [p setX:10 andY:10];
+    
+    r = [Rectangle new];
+    [r setWidth:4 height:4];
+    [r setOrigin:p];
+    
+    [r print];
+    NSLog(@"The rectangle origin is: %@", r.origin);
+    
+    s = [Square new];
+    [s setSide:5];
+    [s print];
+}
+
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        Fraction *aF;
-        Fraction *bF;
-        Fraction *cF;
-        Point2D *p;
-        Rectangle *r;
-        Square *s;
         
-        NSLog(@"Fraction Count: %i", [Fraction count]);
-        //myFraction = [[Fraction alloc] init];
-        aF = [[Fraction allocF] init];
-        NSLog(@"Fraction Count: %i", [Fraction count]);
-        bF = [[Fraction allocF] init];
-        NSLog(@"Fraction Count: %i", [Fraction count]);
-        [aF setTo:6 over:8];
-        [bF setTo:5 over:9];
-        //myFraction.numerator = 1.0;
-        //myFraction.denominator = 3.0;
+        NSLog(@"\nTest Numbers start...");
+        testNumbers();
         
-        [aF print];
-        
-        [aF reduce];
-        
-        [aF print];
-        
-        cF = [aF add: bF];
-        
-        [cF print];
-        
-        p = [Point2D new];
-        [p setX:10 andY:10];
-        
-        r = [Rectangle new];
-        [r setWidth:4 height:4];
-        [r setOrigin:p];
-        
-        [r print];
-        NSLog(@"The rectangle origin is: %@", r.origin);
-        
-        s = [Square new];
-        [s setSide:5];
-        [s print];
+        NSLog(@"\nTest Shapes start...");
+        testShapes();
         
         
     }
